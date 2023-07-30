28e5250b067d0f626bc0560d3597b32b7e9d908e
Ben Richardson <benrichardson@bens-air.lan>
2023-07-30 12:18:41 -0500

    added Aileens code

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,34 @@
+-- Creating the category table
+CREATE TABLE category (
+  category_id VARCHAR PRIMARY KEY,
+  category VARCHAR
+);
+-- Creating the subcategory table
+CREATE TABLE category (
+  subcategory_id VARCHAR PRIMARY KEY,
+  sub-category VARCHAR
+);
+-- Creating the contacts table
+CREATE TABLE contacts (
+  contact_id VARCHAR PRIMARY KEY,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  email VARCHAR
+);
+--Creating the campaign table
+CREATE TABLE campaign (
+  cf_id INT PRIMARY KEY,
+  contact_id VARCHAR,
+  company_name VARCHAR,
+  description VARCHAR,
+  goal NUMERIC,
+  pledged NUMERIC,
+  outcome VARCHAR,
+  bankers_count NUMERIC,
+  country VARCHAR,
+  currency VARCHAR,
+  launched_date DATE,
+  end_date DATE,
+  category_id VARCHAR,
+  subcategory_id VARCHAR
+);
